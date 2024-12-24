9859c99bf89d1356bb55e1c1991036440bf6c35e
obedmacallums <obedmacallums@gmail.com>
2024-12-24 12:42:54 -0300

    Update edit post URL in config to reflect new repository

--- src/config.ts
+++ src/config.ts
@@ -13,7 +13,7 @@ export const SITE: Site = {
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   editPost: {
-    url: "https://github.com/satnaing/astro-paper/edit/main/src/content/blog",
+    url: "https://github.com/obedmacallums/obedmacallums.com/edit/main/src/content/blog",
     text: "Suggest Changes",
     appendFilePath: true,
   },
